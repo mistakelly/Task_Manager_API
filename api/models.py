bd3fbec2c7a0e61bd9cfc11c65c448a3df2a4bfa
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-05-10 10:34:42 +0100

    Added Task Model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,45 @@
+# MODEL
 from django.db import models
+from django.contrib.auth.models import User
+
+# PERFORMANCE
+from django.utils.translation import gettext_lazy as _
+
+# ENCRYPTION
+import binascii
+import os
+
+# TIME
+from django.utils import timezone
+from datetime import timedelta
+
+# VALIDATORS
+from django.core.validators import MinLengthValidator
+
+
+# Task Model
+class Task(models.Model):
+    """
+    A model representing a task with a title, content, status, and timestamps.
+    """
+    TASK_STATUS = [
+        ('completed', 'Completed'), 
+        ('in-progress', 'In-progress')
+    ]
+    title       = models.CharField(max_length=100)
+    content     = models.TextField()
+    created_at  = models.DateTimeField(default=timezone.now)
+    updated_at  = models.DateTimeField(auto_now=True)
+    status      = models.CharField(max_length=20, choices=TASK_STATUS, default='in-progress')
+    owner       = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    # def save(self, **kwargs):
+    #     pass
+
+
+    def __str__(self) -> str:
+        """Return the title of the task."""
+        return self.title
