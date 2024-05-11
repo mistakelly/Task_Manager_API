2224322809a85eb3a302a918a656b75f1ee6366e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-05-11 10:34:42 +0100

    feat: Implemented Refresh Token Logic

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.register, name='register_user'),  # for registering a user
     path('login/', views.login, name='register_user'),  # for logging a user   
     path('tasks/', views.task_manager, name='task_manager'),  # All tasks (GET for list, POST for create) 
+    path('refresh_token/', views.refresh_token, name='register_user'),  # for logging a user 
 ]
