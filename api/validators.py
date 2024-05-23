af96f6e395533b5580bcd7ee3f725c93acce142d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-05-23 01:34:56 +0100

    created a custom validation function for task inputs

--- api/validators.py
+++ api/validators.py
@@ -19,3 +19,29 @@ def validate_user_inputs(request):
 
     
     return username, password
+
+
+# VALIDATE TASK INPUTS
+def validate_task_inputs(request):
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid Json Format'}, status=400)
+    
+     # validate and Extract data using python warlus operator.
+    if not (title := data.get('title')):
+        return JsonResponse({'error': 'Title cannot be null or empty.'}, status=400)
+
+    if not (description := data.get('description')):
+        return JsonResponse({'error': 'Description cannot be null or empty.'}, status=400)
+
+    status = data.get('status')
+
+    if status not in ['in-progress', 'completed', 'pending']:
+        return JsonResponse({'error': 'Invalid status. Must be one of: in-progress, completed, pending.'}, status=400)
+
+    if not (owner_id := data.get('owner')):
+        return JsonResponse({'error': 'Owner ID cannot be null or empty.'}, status=400)
+    
+
+    return title, description, status, owner_id
